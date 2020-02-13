f578cf961a954ab6ab775b7dbfe40cf9fb7ec16e
Michael Cooper <mcooper628@gmail.com>
2020-02-12 22:52:58 -0800

    more formatting

--- src/components/GithubActivity.js
+++ src/components/GithubActivity.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { List } from "semantic-ui-react";
 import axios from "axios";
 
 // export default axios.create({
@@ -7,43 +8,58 @@ import axios from "axios";
 // });
 
 const GithubActivity = () => {
-  const [data, setData] = useState({ hits: [] });
+  //const [data, setData] = useState({ hits: [] });
   const [git, setGit] = useState({ data: [] });
   useEffect(() => {
     const fetchData = async () => {
       const git = await axios(
         "https://api.github.com/users/MichaelDCooper/events/public"
       );
-      const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=linux"
-      );
+      // const result = await axios(
+      //   "https://hn.algolia.com/api/v1/search?query=linux"
+      // );
 
-      //console.log(git.data, result.data.hits);
+      console.log(git.data);
       setGit(git);
-      setData(result.data);
+      //setData(result.data);
     };
     fetchData();
   }, []);
   return (
     <div>
-      <ul>
-        {/* {console.log(data.hits)} */}
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
-
-      <ul>
+      <List divided relaxed>
         {git.data.map(item => (
-          <li key={item.id}>{item.type}</li>
+          <List.Item key={item.id}>
+            <List.Icon name="github" size="large" verticalAlign="middle" />
+            <List.Content>
+              <List.Header
+                as="a"
+                href={"https://github.com/" + item.repo.name}
+                key={item.id}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.repo.name}
+              </List.Header>
+              <List.Description as="a">
+                {item.type + " Updated: " + item.created_at}
+              </List.Description>
+            </List.Content>
+          </List.Item>
         ))}
-        {console.log(git.data)}
-        <li>fuck</li>
-      </ul>
+      </List>
     </div>
   );
 };
 
 export default GithubActivity;
+
+// {/* <ul>
+// {/* {console.log(data.hits)} */}
+// {data.hits.map(item => (
+//   <li key={item.objectID}>
+//     <a href={item.url}>{item.title}</a>
+//   </li>
+// ))}
+// </ul>
+// <ul> */}
